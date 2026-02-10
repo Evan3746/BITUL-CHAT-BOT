7aa0dcb5364a5e714b6414f837fc797f26466219
Evan3746 <mrevan349900@gmail.com>
2026-02-10 20:15:14 +0600

    Update album.js

--- Script/commands/album.js
+++ Script/commands/album.js
@@ -13,14 +13,14 @@ module.exports.config = {
  name: "album",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "Dipto Modified By SAHU", 
+ credits: "MR BITUL", 
  description: "Displays album options for selection.",
  usePrefix: true,
  prefix: true,
  category: "Media",
  commandCategory: "Media",
  usages:
- "Only or add [cartoon/photo/lofi/sad/islamic/funny/horny/anime/aesthetic/cat/lyrics/love/sigma]",
+ "Only or add [cartoon/photo/lofi/sad/islamic/funny/horny/sex/anime/aesthetic/cat/lyrics/love/sigma]",
  cooldowns: 5,
 };
 
@@ -148,7 +148,7 @@ module.exports.run = async function ({ api, event, args }) {
 
 module.exports.handleReply = async function ({ api, event, handleReply }) {
  api.unsendMessage(handleReply.messageID);
- const admin = "100001039692046";
+ const admin = "100008551535859";
 
  if (event.type !== "message_reply") return;
 
